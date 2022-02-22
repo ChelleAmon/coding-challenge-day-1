036bc5134831ce7e08cbf47e5639a4f552225a8f
Gichelle Amon <amongichelle@gmail.com>
2022-02-22 10:03:49 -0800

    feat(schema): setup user schema

---
+++ src/server/schemas/user.schema.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import type {User} from '../../shared/user.model.js';
+const {Schema, model} = mongoose;
+const opts = {toJSON: {virtuals: true}}
+
+const userSchema = new Schema<User>({
+    first_name: {type: String},
+    last_name: {type: String},
+    age: {type: Number},
+}, opts)
+
+userSchema.virtual('intro').get(function(this: any){
+    return `${this.first_name} ${this.last_name} is ${this.age} years old.`
+})
+
+export const UserModel = model<User>('User', userSchema)
+
+
