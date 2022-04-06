d8601bfeb20c336f8f6d03c601930307827ac57c
Gichelle Amon <amongichelle@gmail.com>
2022-04-06 09:59:34 -0700

    challenge completed

---
+++ JS/0406.js
@@ -0,0 +1,30 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findInteger(nums){
+    let map = {}
+
+    for(let i = 0; i < nums.length; i++){
+        map[nums[i]] = map[nums[i]] + 1 || 1
+    }
+
+    for(let prop in map){
+        if (map[prop] % 2 !== 0){
+            console.log(prop)          
+        }
+    }
+}
+
+
+findInteger([7, 8, 8])
+findInteger([1,2,2,3,3,3,4,3,3,3,2,2,1])
+
